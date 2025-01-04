07904d8b96061f31bf84449e94a558a15d5c3b89
Imran <imgha@hotmail.com>
2025-01-04 18:49:01 +0100

    m

--- routers/logout.ts
+++ routers/logout.ts
@@ -3,7 +3,12 @@ import express from 'express';
 const LOGOUT_SESSION = express.Router();
 
 LOGOUT_SESSION.post('/logout', (request, response) => {
-	response.clearCookie('session'); 
+	response.clearCookie('session', {
+		path: '/',
+		httpOnly: true,
+		secure: true,
+		sameSite: 'none',
+	  });
 	response.status(200).json();
 });
 
