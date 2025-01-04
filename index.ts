dbaa7365b4daa72af1647f5d1a0ac8f81e158315
Imran <imgha@hotmail.com>
2025-01-04 18:10:48 +0100

    m

--- index.ts
+++ index.ts
@@ -8,45 +8,38 @@ import { connectDB } from './server/mongo.db';
 
 dotenv.config();
 
-// Initialiseer express app
 const APP: Express = express();
 
-// View-engine configuratie
 APP.set('view engine', 'ejs');
 APP.set('views', path.join(__dirname, 'views'));
 
-// Middleware
 APP.use(express.json({ limit: '10mb' }));
 APP.use(express.urlencoded({ limit: '10mb', parameterLimit: 50, extended: true }));
 APP.use(express.static(path.join(__dirname, 'public')));
 APP.use(cookieParser());
 APP.use(cors({
-  origin: 'https://teach-me-ui.vercel.app', // Sta alleen requests toe van jouw frontend
-  credentials: true, // Sta cookies toe
+  origin: 'https://teach-me-ui.vercel.app', 
+  credentials: true, 
 }));
 
-// Verbinden met database bij serverstart
 (async () => {
   try {
     await connectDB();
     console.log('Succesvol verbonden met de database');
   } catch (error) {
     console.error('Databaseverbinding mislukt:', error);
-    process.exit(1); // Sluit de server als de database niet verbindt
+    process.exit(1); 
   }
 })();
 
-// Routes
 APP.use('/api', APIROUTER);
 
-// Foutafhandeling voor niet-bestaande routes
 APP.use((req, res) => {
   res.status(404).json({
     error: 'The specified path does not exist.',
   });
 });
 
-// Start de server
 if (!process.env.VERCEL) {
   const PORT = process.env.PORT || 3000;
   APP.listen(PORT, () => {
